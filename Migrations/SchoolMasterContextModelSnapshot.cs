cde9e2a7eab888776f9160ecd755af1d31c98c27
Damilola Adeniyi <adeniyidamilola246@gmail.com>
2026-06-26 12:28:28 +0100

    Add new migration files for AcademicYears, Classes, Subjects, Tenants, Terms, Periods, Users, Staff, and Students

--- Migrations/SchoolMasterContextModelSnapshot.cs
+++ Migrations/SchoolMasterContextModelSnapshot.cs
@@ -419,6 +419,9 @@ namespace SchoolMaster.Migrations
                         .IsUnique()
                         .HasFilter("\"IsCurrent\" = true");
 
+                    b.HasIndex("TenantId", "AcademicYearId", "TermNumber")
+                        .IsUnique();
+
                     b.ToTable("Terms");
                 });
 
